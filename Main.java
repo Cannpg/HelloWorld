7c2ae961c42529546fe37bf0ab626db33e16d14d
cannpg <80770043+Cannpg@users.noreply.github.com>
2021-08-26 13:24:28 +0000

    Thurs in-class assignment commented in

--- Main.java
+++ Main.java
@@ -2,5 +2,29 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("My name is Parker Cann");
+
+
+  // This was my in-class drawing for Thurs
+  /*String cool = 
+
+    "      /\\      \n" +
+    "     /  \\     \n" +
+    "    /    \\    \n" +
+    "   /   |  \\   \n" +
+    "  |    |   |   \n" +
+    "  |    |   |   \n" +
+    "  \\    \\  /  \n" +
+    "   \\    \\/   \n" +
+    "    \\    \\   \n" +
+    "    /\\    \\  \n" +
+    "   /  \\    |  \n" +
+    "  |    |   |   \n" +
+    "  \\    |   /  \n" +
+    "   \\   |  /   \n" +
+    "    \\    /    \n" +
+    "     \\  /     \n" +
+    "      \\/      \n";
+
+    System.out.println(cool);*/
   }
 }
